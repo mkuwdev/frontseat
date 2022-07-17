223ab9f6e2af7fb45cd83404370e4aa4084f55b4
[your <alessandrofigo24@gmail.com>
2022-07-17 20:34:58 +0700

    fixed body spacing

--- src/components/SiteLayout.js
+++ src/components/SiteLayout.js
@@ -5,7 +5,7 @@ const SiteLayout = ({ children }) => {
     return (
       <div className="flex flex-col min-h-screen">
           <Navbar/>
-          <div className="max-w-7xl px-2 py-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl w-full px-2 py-2 sm:px-6 lg:px-8">
             {children}
           </div>
       </div>
